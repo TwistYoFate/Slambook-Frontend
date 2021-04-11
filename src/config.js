f51453c6f7a5eaafb332b42b656940022b3989a0
Deepanshu Yadav <daybond007@gmail.com>
2021-04-12 01:23:22 +0530

    changed backend-api from https to http

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
     // SERVER_BASE_URL:"http://localhost:5000"        //development
-    SERVER_BASE_URL:"https://65.1.199.197:5000"     //production
+    SERVER_BASE_URL:"http://65.1.199.197:5000"     //production
 }
